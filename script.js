59fd8c67e9401b0660586bbe602352b85f9f7fa2
Lucas <lafdesouza2002@outlook.com>
2020-07-20 01:42:47 -0300

    Initializing the project

---
+++ script.js
@@ -0,0 +1,11 @@
+let number = document.getElementById('number')
+
+function converter(){
+    console.log(number.value)
+    let result = number.value/24
+    let p = document.createElement("p")
+    p.innerHTML = result
+    document.body.appendChild(p)
+}
+
+initializing 
